aad89e41a2a22daecec488c2ccb1da8ada547d7e
Sayyam <sayyammshah@gmail.com>
2022-05-29 18:20:33 +0530

    All Interfaced Done

--- app/src/main/java/com/example/oliomart/fragments/AccountFragment.java
+++ app/src/main/java/com/example/oliomart/fragments/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.oliomart.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.example.oliomart.LoginActivity;
+import com.example.oliomart.MainActivity;
 import com.example.oliomart.R;
+import com.example.oliomart.config.SharedPreferenceClass;
 
 public class AccountFragment extends Fragment {
 
     View view;
+    Button logoutBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_account, container, false);
+        logoutBtn = view.findViewById(R.id.Logout_Btn);
 
-        // Inflate the layout for this fragment
+        SharedPreferenceClass spClassAccounts = new SharedPreferenceClass(getContext());
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spClassAccounts.editor.clear();
+                spClassAccounts.editor.commit();
+                Intent i = new Intent(getContext(), LoginActivity.class);
+                i.putExtra("finish", true);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
+
         return view;
     }
 }
